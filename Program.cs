4279c7e3b3a69ff9f4c94e84ad17fc2ef82454bc
Alena <weaterwind@gmail.com>
2023-04-01 20:38:47 +0700

    Add task 21

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿// Задача 19
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
-Console.Write("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("x1 = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y1 = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z1 = ");
+double z1 = Convert.ToDouble(Console.ReadLine());
 
-int n1 = n / 10000;
-int n2 = n / 1000 % 10;
-int n4 = n / 10 % 10;
-int n5 = n % 10;
+Console.Write("x2 = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y2 = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z2 = ");
+double z2 = Convert.ToDouble(Console.ReadLine());
 
-if(n1 == n5 && n2 == n4)
-    Console.WriteLine("Палиндром");
-else
-    Console.WriteLine("Не палиндром");
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
